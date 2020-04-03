7a14833df2fb90b4a552474bc01ce9f7820b781b
tichaves <tiagochaves90@gmail.com>
2020-04-03 19:34:25 +0100

    Tiago 19h34

--- app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/Activities/HomePageActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/Activities/HomePageActivity.java
@@ -1,12 +1,18 @@
 package pt.ulisboa.tecnico.sise.lab03.dummynotepad.Activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
+import pt.ulisboa.tecnico.sise.lab03.dummynotepad.DataModel.Claim;
+import pt.ulisboa.tecnico.sise.lab03.dummynotepad.InternalProtocol;
 import pt.ulisboa.tecnico.sise.lab03.dummynotepad.R;
 
 public class HomePageActivity extends AppCompatActivity {
@@ -16,6 +22,7 @@ public class HomePageActivity extends AppCompatActivity {
     private Button buttonSettings;
     private Button buttonNewClaim;
     private Button buttonHelp;
+    private ArrayList<Claim> claimList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +35,7 @@ public class HomePageActivity extends AppCompatActivity {
         buttonNewClaim  = (Button)  findViewById(R.id.home_page_new_claim_btn);
         buttonHelp      = (Button)  findViewById(R.id.home_page_help_btn);
 
-        /*buttonMyProfile.setOnClickListener(new View.OnClickListener() {
+        buttonMyProfile.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d(LOG_TAG, "MyProfile debug message!");
 
@@ -59,7 +66,7 @@ public class HomePageActivity extends AppCompatActivity {
 
                 startActivity(intent);
             }
-        });*/
+        });
 
         buttonNewClaim.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -71,6 +78,38 @@ public class HomePageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case InternalProtocol.NEW_CLAIM_REQUEST:
+
+                if (resultCode == Activity.RESULT_OK) {
+
+                    // retrieve the title and body of the note from the intent
+                    String claimTitle = data.getStringExtra(InternalProtocol.KEY_NEW_CLAIM_TITLE);
+                    String claimPlate = data.getStringExtra(InternalProtocol.KEY_NEW_CLAIM_PLATE);
+                    String claimDate = data.getStringExtra(InternalProtocol.KEY_NEW_CLAIM_DATE);
+                    String claimDescription = data.getStringExtra(InternalProtocol.KEY_NEW_CLAIM_DESCRIPTION);
+
+                    // update the domain data structures
+                    this.claimList.add(new Claim(claimTitle, claimPlate, claimDate, claimDescription));
+/*
+                    // refresh the list on screen
+                    _listView.setAdapter(new ArrayAdapter<>(this,
+                            android.R.layout.simple_list_item_1, android.R.id.text1, _noteList));*/
+
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    Log.d(InternalProtocol.LOG, "Cancel pressed.");
+                } else {
+                    Log.d(InternalProtocol.LOG, "Internal error: unknown result code.");
+                }
+                break;
+            default:
+                Log.d(InternalProtocol.LOG, "Internal error: unknown intent message.");
+        }
     }
 
 }
---
+++ app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/DataModel/Claim.java
@@ -0,0 +1,39 @@
+package pt.ulisboa.tecnico.sise.lab03.dummynotepad.DataModel;
+
+public class Claim {
+    private String title;
+    private String plate;
+    private String date;
+    private String description;
+    public Claim(String title, String plate, String date, String description) {
+        this.title = title;
+        this.plate = plate;
+        this.date = date;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlate() { return plate; }
+
+    public void setPlate(String plate) { this.plate = plate; }
+
+    public String getDate() { return date; }
+
+    public void setDate(String date) { this.date = date; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getDescription() { return description; }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
+}
