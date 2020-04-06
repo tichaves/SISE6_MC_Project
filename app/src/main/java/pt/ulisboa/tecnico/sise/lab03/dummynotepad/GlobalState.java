6eb6e896bf7b6932e207952bcaa44033ed145c5a
tichaves <tiagochaves90@gmail.com>
2020-04-06 12:56:41 +0100

    Tiago 13h

--- app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/GlobalState.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/GlobalState.java
@@ -3,22 +3,11 @@ package pt.ulisboa.tecnico.sise.lab03.dummynotepad;
 import android.app.Application;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import pt.ulisboa.tecnico.sise.lab03.dummynotepad.DataModel.Claim;
-import pt.ulisboa.tecnico.sise.lab03.dummynotepad.DataModel.Note;
 
 public class GlobalState extends Application {
-    private ArrayList<Note> _noteList;
     private int sessionId;
-
-    public void setNoteList(ArrayList<Note> noteList) {
-        _noteList = noteList;
-    }
-
-    public ArrayList<Note> getNoteList() {
-        return _noteList;
-    }
 
     private ArrayList<Claim> claimList = new ArrayList<Claim>();
 
--- app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/InternalProtocol.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/lab03/dummynotepad/InternalProtocol.java
@@ -5,21 +5,13 @@ public class InternalProtocol {
     public static final String SESSION_ID = "SESSION_ID";
 
     // relevant for the login activity
-    public static final int    NEW_NOTE_REQUEST   = 4;
-    public static final String KEY_NEW_NOTE_TITLE = "NOTE_TITLE";
-    public static final String KEY_NEW_NOTE_BODY  = "NOTE_BODY";
-
     public static final int    NEW_CLAIM_REQUEST   = 1;
     public static final String KEY_NEW_CLAIM_TITLE = "CLAIM_TITLE";
     public static final String KEY_NEW_CLAIM_PLATE = "CLAIM_PLATE";
     public static final String KEY_NEW_CLAIM_DATE  = "CLAIM_DATE";
     public static final String KEY_NEW_CLAIM_DESCRIPTION  = "CLAIM_DESC";
 
-    public static String KEY_USER     = "USER_APP";
-    public static String KEY_PASSWORD = "USER_PASSWORD";
-
     // relevant for the read note activity
-    public static final String READ_NOTE_INDEX    = "NOTE_INDEX";
     public static final String READ_CLAIM_INDEX   = "CLAIM_INDEX";
 
     public static final int CLAIM_INFORMATION_REQUEST   = 3;
